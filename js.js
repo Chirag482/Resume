7639a3815b1d58012e335bbcf15cc0268cb52312
Chirag <chiragvalecha97@gmail.com>
2021-02-22 21:53:46 +0530

    skills auto fill

--- js.js
+++ js.js
@@ -20,3 +20,40 @@ for (var i = 0; i < navMenuAnchorTags.length; i++) {
     }, 5);
   });
 }
+
+//auto fill skills section
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementsByClassName("skills-display");
+window.addEventListener("scroll", checkScroll);
+var animationDone = false;
+console.log(skillsContainer);
+function intialiseBars() {
+  for (let bar of progressBars) {
+    bar.style.width = 0 + "%";
+  }
+}
+intialiseBars();
+function checkScroll() {
+  var coordinates = skillsContainer[0].getBoundingClientRect();
+  if (coordinates.top < window.innerHeight) {
+    if (!animationDone) {
+      animationDone = true;
+      fillBars();
+    }
+  }
+}
+
+function fillBars() {
+  for (let bar of progressBars) {
+    let targetWidth = bar.getAttribute("data-bar-width");
+    let currentWidth = 0;
+    let interval = setInterval(function () {
+      if (currentWidth > targetWidth) {
+        clearInterval(interval);
+        return;
+      }
+      currentWidth++;
+      bar.style.width = currentWidth + "%";
+    }, 10);
+  }
+}
